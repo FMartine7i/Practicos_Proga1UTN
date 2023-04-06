d4f5c141b724218fceb7e3fd156fe6c37a375877
Ruined-King <frmmartinetti98@gmail.com>
2023-04-06 14:38:07 -0300

    Ejercicio 7 corregido.

--- Practico_2/Ejercicio_7.cpp
+++ Practico_2/Ejercicio_7.cpp
@@ -3,18 +3,18 @@
 using namespace std;
 
 int main(){
-    float α, β, ɣ, a, b, c, aux;
+    float alfa, beta, gamma, a, b, c, aux;
     b = 4;
-    α = 90*M_PI/180;
-    ɣ = 60*M_PI/180;
-    β = M_PI - α - ɣ;
+    alfa = 90*M_PI/180;
+    gamma = 60*M_PI/180;
+    beta = M_PI - alfa - gamma;
 
-    a = 4*tan(β);
+    a = 4*tan(beta);
     aux = pow(a, 2) + pow(b, 2);
     c = sqrt(aux);
-    β = β*180/M_PI; // conversión para que el programa me devuelva el valor en grados y no en radianes. 
+    beta = beta*180/M_PI; // conversión para que el programa me devuelva el valor en grados y no en radianes. 
 
-    cout << "El valor de β es: " << β << " grados, el valor de a es: " << a << " y el valor de c es: " << c << endl;
+    cout << "El valor de β es: " << beta << " grados, el valor de a es: " << a << " y el valor de c es: " << c << endl;
 
     return 0;
 }
